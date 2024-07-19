65c238e7fef45cfcb14e9c5042b36331dd5234b8
Kristina <sharipovakristina@gmail.com>
2024-07-19 10:56:05 +0300

    LAZY

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -40,7 +40,7 @@ public class User implements UserDetails {
     @Column(name = "year_of_birth")
     private Integer yearOfBirth;
 
-    @ManyToMany(cascade = CascadeType.MERGE)
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
